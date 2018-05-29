6b067f46b6120505541a95254b41d29885cff566
Krafty-Coder <agtychefortuna@gmail.com>
2018-05-30 00:59:44 +0300

    add the initialised jasmine spec and sample of test

---
+++ spec/test_calculation.spec.js
@@ -0,0 +1,20 @@
+describe('calculate addition', function(){
+
+  it('should be able to add two numbers together', function(){
+    console.log('I was able to add two numbers together');
+  });
+
+  it('should be able to add two numbers together', function(){
+    console.log('I was able to add more than two numbers together');
+  });
+
+  describe('calculate addition with negative numbers', function(){
+
+    it('it should be able to add two negative numbers together', function(){
+      console.log('Yaaay, just added two negative numbers');
+    });
+
+  });
+
+});
+
